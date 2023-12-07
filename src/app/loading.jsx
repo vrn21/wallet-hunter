6213364baabee2e7ca0a93ae12c99a06e9465d01
K V Varun Krishnan <varunkkv.mec@gmail.com>
2023-12-07 13:53:08 +0530

    Update: Added Footer section

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,28 +1,55 @@
 'use client'
+// import React from "react";
+// import {CircularProgress} from "@nextui-org/react";
+
+// export default function App() {
+//   //const [value, setValue] = useState(0);
+
+//   // React.useEffect(() => {
+//   //   const interval = setInterval(() => {
+//   //     setValue((v) => (v >= 100 ? 0 : v + 10));
+//   //   }, 500);
+
+//   //   return () => clearInterval(interval);
+//   // }, []);
+
+//   // return (
+//   //   <CircularProgress
+//   //     aria-label="Loading..."
+//   //     size="lg"
+//   //     value={value}
+//   //     color="warning"
+//   //     showValueLabel={true}
+//   //   />
+//   // );
+//   return(
+//   <h1>Loading...</h1>
+//   )
+// }
 import React from "react";
 import {CircularProgress} from "@nextui-org/react";
 
 export default function App() {
-  //const [value, setValue] = useState(0);
+  const [value, setValue] = React.useState(0);
 
-  // React.useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setValue((v) => (v >= 100 ? 0 : v + 10));
-  //   }, 500);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((v) => (v >= 100 ? 0 : v + 10));
+    }, 500);
 
-  //   return () => clearInterval(interval);
-  // }, []);
+    return () => clearInterval(interval);
+  }, []);
 
-  // return (
-  //   <CircularProgress
-  //     aria-label="Loading..."
-  //     size="lg"
-  //     value={value}
-  //     color="warning"
-  //     showValueLabel={true}
-  //   />
-  // );
-  return(
-  <h1>Loading...</h1>
-  )
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Loading</h1>
+    <CircularProgress
+      aria-label="Loading..."
+      size="lg"
+      value={value}
+      color="warning"
+      showValueLabel={true}
+      />
+      </div>
+  );
 }
